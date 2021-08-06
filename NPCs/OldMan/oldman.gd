d5db9b3a8508261bdc315d2a4eeb504823ce13f4
DanielSong128 <dannynt2013@gmail.com>
2021-08-06 14:46:41 -0500

    Added Princess and Old Man Idle Animations (In tutorial scene for animation testing can remove whenever)
    - Made gd, tres, tscn for Princess and Old Man
    - Added Idle Animations for both
    - Placed them in tutorial scene for testing (can remove whenever you want)

---
+++ NPCs/OldMan/oldman.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var anim_player = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	anim_player.play ("Idle")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
