91e51928fa256091938405573d7bc03642548bab
Tyler Funk <tcfunk25@gmail.com>
2021-08-12 15:37:06 -0500

    add extra time sprite and scene

---
+++ time_powerup/extra_time.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		body.increase_coin_collected_count()
+		queue_free()
